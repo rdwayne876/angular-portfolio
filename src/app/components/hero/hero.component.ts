4798f596f4121a23c03651825ad8425409b63d03
Dwayne Robinson <rdwayne876@gmail.com>
2023-04-08 19:20:52 -0500

    Hero section cont'd

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { styles } from 'src/styles';
+import * as THREE from 'three'
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit{
+
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
+  styles = styles
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  renderer = new THREE.WebGLRenderer({ canvas: this.canvasRef.nativeElement})
+
+  ngOnInit(): void{
+    this.renderer.setSize(window.innerWidth, window.innerHeight )
+  }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,15 @@ module.exports = {
       colors: {
         primary: '#7FDBFE',
         secondary: '#344A53',
-        tertiary: '#97AFB9'
+        tertiary: '#97AFB9',
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
       },
       backgroundImage:{
-        "hero-pattern": "url('src/assets/magicpattern-pattern-1680314440548.jpeg')"
+        "hero-pattern": "url('/assets/magicpattern-pattern-1680315419968.jpeg')"
+        // "hero-pattern": "url('/assets/magicpattern-pattern-1680315445018.jpeg')"
+
       }
     },
   },
